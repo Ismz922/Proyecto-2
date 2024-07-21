c620b886b3871542ae85dfb888e6d78b329dca8e
Ismz922 <isaacsalazarmolina0@gmail.com>
2024-07-20 20:15:35 -0600

    Add files via upload

--- js/Grafico.js
+++ js/Grafico.js
@@ -1,27 +1,22 @@
-$(document).ready(function nose(){
-    const Nombres=[];
-const Sucursales=[];
-for (let index = 0; index < tiendas.length; index++) {
-    Nombres.add(tiendas.Tiendas[index].nombre);
-    Sucursales.add(tiendas.Tiendas[index].numero);
-    
-};
-})
-let canva = document.getElementById("Grafico").getContext("2d");
-
-var chart = new Chart(canva, {
-  type: "bar",
-  data: {
-    labels: ["Alajuela", "San Jose", "Heredia", "Limon", "Guanacaste", "Puntarenas", "Cartago"],
-    datasets: [
-      {
-        
-        label: ["Sucursales en el país"],
-        backgroundColor:["#BE0A0A","#9B0ABE","#E6FB14","#19EC0F","#C4981A","#FF9311","#023CF3"],
-     
-        data:[3,5,2,1,1,2,1]
-       
-      }
-    ]
-  }
-})
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('Json/Sucursales.json')
+        .then(response => response.json())
+        .then(datas => {
+            let canva = document.getElementById("Grafico").getContext("2d");
+            var chart = new Chart(canva, {
+              type: "bar",
+              data: {
+                labels:datas.labels,
+                datasets: [
+                  {
+                    label: "Sucursales en el país    ",
+                    backgroundColor:["#BE0A0A","#9B0ABE","#E6FB14","#19EC0F","#C4981A","#FF9311","#023CF3"],
+                    
+                    data:datas.data
+                  }
+                ]
+              }
+            })
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error));
+});
