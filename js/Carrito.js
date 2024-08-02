877a273ef3e47343b2d29d3ca6bb37ba315305a2
Ismz922 <isaacsalazarmolina0@gmail.com>
2024-08-02 15:47:30 -0600

    Add files via upload

---
+++ js/Carrito.js
@@ -0,0 +1,23 @@
+const urlParams = new URLSearchParams(window.location.search);
+const productoId = urlParams.get("id");
+const producto = productos.find((p) => p.id == productoId)
+const compraBoton = document.getElementById("Agregar");
+function Guardar() {
+  const id=producto.id
+  const nombre=producto.nombre
+  const precio=producto.precio
+
+    const enviar = {
+    id,
+    nombre,
+    precio
+    };
+    
+    saveContactLocalStorage(enviar);
+  };
+  const saveContactLocalStorage = (enviar) => {
+    let envios = JSON.parse(localStorage.getItem("envios")) || [];
+    envios.push(enviar);
+    localStorage.setItem("envios", JSON.stringify(envios));
+
+  };
