86e9beff3fe0b6f8fc313beee14238223683d2a9
Ismz922 <isaacsalazarmolina0@gmail.com>
2024-08-16 14:39:36 -0600

    Update Cargar-Factura.js

--- js/Cargar-Factura.js
+++ js/Cargar-Factura.js
@@ -1,10 +1,19 @@
 function Carga() {
-    let facturas = JSON.parse(localStorage.getItem("facturas")) || [];
-    var texto = document.getElementById('Dinero')
-    var total=document.getElementById('Total')
-    for (let index = 0; index < facturas[0].subtotales.length; index++) {
-        var parrafo = document.createElement("tr")
-        parrafo.innerHTML = `<tr>
+  let facturas = JSON.parse(localStorage.getItem("facturas")) || [];
+  var texto = document.getElementById('Dinero')
+  var fecha = document.createElement('h2')
+  const fechaActual = new Date();
+  const dia = fechaActual.getDate();
+  const mes = fechaActual.getMonth() + 1; 
+  const anio = fechaActual.getFullYear();
+
+  
+  fecha.innerHTML = `Fecha de la factura: ${dia}/${mes}/${anio}`
+  
+  var total = document.getElementById('Total')
+  for (let index = 0; index < facturas[0].subtotales.length; index++) {
+    var parrafo = document.createElement("tr")
+    parrafo.innerHTML = `<tr>
          <td>
               <p>${facturas[0].elementos[index]}</p>
             </td>
@@ -15,19 +24,20 @@ function Carga() {
               <p  style="margin-left: 10em;">$${facturas[0].subtotales[index]}</p>
             </td>
             </tr> `
-texto.appendChild(parrafo)
+    texto.appendChild(parrafo)
 
-    }
-    var totalColocar=document.createElement('h2')
-    var totalColocar2=document.createElement('h2')
-    var totalColocar3=document.createElement('h2')
-    totalColocar.innerHTML=`Total: $${facturas[0].total}`
-    total.appendChild(totalColocar)
-    totalColocar3.innerHTML=`Envío: ${facturas[1]}`
-    total.appendChild(totalColocar3)
-    totalColocar2.innerHTML=`Tarjeta: ${facturas[2]}`
-    total.appendChild(totalColocar2)
+  }
+  total.appendChild(fecha)
+  var totalColocar = document.createElement('h2')
+  var totalColocar2 = document.createElement('h2')
+  var totalColocar3 = document.createElement('h2')
+  totalColocar.innerHTML = `Total: $${facturas[0].total}`
+  total.appendChild(totalColocar)
+  totalColocar3.innerHTML = `Envío: ${facturas[1]}`
+  total.appendChild(totalColocar3)
+  totalColocar2.innerHTML = `Tarjeta: ${facturas[2]}`
+  total.appendChild(totalColocar2)
 }
 document.addEventListener('DOMContentLoaded', function () {
-    Carga();
+  Carga();
 });
