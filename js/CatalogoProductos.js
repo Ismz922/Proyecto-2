8befad6254ec381e4263c7c5890ac6ad0c874b37
DerianCD <deriancd@gmail.com>
2024-08-17 14:03:41 -0600

    Add files via upload

--- js/CatalogoProductos.js
+++ js/CatalogoProductos.js
@@ -16,17 +16,18 @@ function mostrarProductos(data){
     data.forEach((producto) => {
 
         const productoAgregar = ` 
-        <div class="col">
-            <div class="card h-100">
-                <img class="card-img-top" src="${producto.imagenes[0]}" alt="${producto.imagenes[0]}">
-                <div class="card-body ">
-                    <h5 class="card-title ">${producto.nombre}</h5>
-                    <p class="card-text fs-4 mt-2">$${producto.precio}</p>
-                </div>
-                <div class="card-footer d-flex justify-content-center">
-                    <button type="button" class="btn btn-primary" style="width:80%;" onClick="DetalleProducto(${producto.id})">Detalles</button>
-                </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-12 my-2">
+          <div class="card h-100">
+            <img class="card-img-top" src="${producto.imagenes[0]}" alt="${producto.imagenes[0]}">
+            <div class="card-body ">
+              <h5 class="card-title ">${producto.nombre}</h5>
+              <p class="card-text fs-4 mt-2">$${producto.precio}</p>
             </div>
+            <div class="card-footer d-flex justify-content-center">
+              <button type="button" class="btn btn-primary" style="width:80%;"
+                onClick="DetalleProducto(${producto.id})">Detalles</button>
+            </div>
+          </div>
         </div>`
         $('#tienda').append(productoAgregar)
     })
