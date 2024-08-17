4c39998a54804d96ec29a766c4cf9dd70efcad72
Ismz922 <isaacsalazarmolina0@gmail.com>
2024-08-17 14:54:19 -0600

    Update Carrito.js

--- js/Carrito.js
+++ js/Carrito.js
@@ -7,23 +7,41 @@ function Guardar() {
   const nombre=producto.nombre
   const precio=producto.precio
   const imagenes=producto.imagenes
+  const stock=producto.stock
 
     const enviar = {
     id,
     nombre,
     precio,
-imagenes
+imagenes,
+stock
     };
-    var texto= document.createElement("b")
-    texto.innerHTML="<br><br> Se a agregado el producto correctamente al carrito "
-    texto.style.color="green"
-    texto.style.fontSize="20px"
-    compraBoton.appendChild(texto)
     saveContactLocalStorage(enviar);
+    $.notify("Se ha añadido el producto al carrito", "success");
   };
   const saveContactLocalStorage = (enviar) => {
-    let envios = JSON.parse(localStorage.getItem("envios")) || [];
-    envios.push(enviar);
-    localStorage.setItem("envios", JSON.stringify(envios));
+   
+    var envios = new Array()
+	if (localStorage.getItem('envios')) {
+		envios = JSON.parse(localStorage.getItem('envios'))
+	}
+	if(envios.length>0){
+		let itemIndex = envios.findIndex((obj) => obj.id == enviar.id);
+		
+		if(itemIndex != -1){
+			envios[itemIndex]=enviar
+		}else{
+			
+			envios.push(enviar)
+		}
+		
+	}else{
+		
+    envios.push(enviar)
+		
+	}
+	
+	localStorage.setItem('envios',  JSON.stringify(envios))
+  
 
   };
