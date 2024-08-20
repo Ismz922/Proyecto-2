cace5bdea7b8ca5ccc0592f57f4ad1ef89fa41f0
Ismz922 <isaacsalazarmolina0@gmail.com>
2024-08-20 14:31:35 -0600

    Update Validacioens.js

--- js/Validacioens.js
+++ js/Validacioens.js
@@ -5,11 +5,21 @@ document.getElementById('Formulario').addEventListener('submit', function(event)
    
     
     const textValue = textInput.value.trim(); 
+    const correo = document.getElementById('email');
+   
+    
+    const correoValor = correo.value.trim(); 
     
     if (textValue === '') {
      
        $.notify("El espacio del nombre está vacío", "warn");
+       return;
     } 
+    if (correoValor === '') {
+     
+        $.notify("El espacio del correo está vacío", "warn");
+        return;
+     } 
     const dateInput = document.getElementById('Fecha');
    
     
@@ -18,6 +28,19 @@ document.getElementById('Formulario').addEventListener('submit', function(event)
     if (dateValue === '') {
         
         $.notify("No ha selecionado una fecha valida", "warn");
+        return;
     } 
-        
+    const hoy = new Date();
+    const fechaNac = new Date(dateValue);
+    let edad = hoy.getFullYear() - fechaNac.getFullYear();
+    const mes = hoy.getMonth() - fechaNac.getMonth();
+
+    if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNac.getDate())) {
+        edad--;
+    }
+    if (edad<16) {
+        $.notify("Por favor pedirle a un adulto que llene el formulario ya que se detectó que el usuario es menor de edad", "warn");
+        return;
+    }
+    $.notify("Se ha enviado el formulario correctamente", "success");
 });
